8426f4bda97c6ddf54f1abe238d18a8e7d9faa79
Martial <martial.tarizzo@gmail.com>
2024-07-02 14:17:27 +0200

    V 2.6.1 - correctif
    Le local storage n'est pas accessible depuis le service worker
    =>
    retrait du code incorrect dans le SW
    	modified:   service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,11 +9,7 @@
 
 // Le n° de version de l'appication PWA
 // Mettre à jour pour forcer l'actualisation du cache du navigateur
-const VERSION_SC = "2.6.0"
-
-// Enregistrement du n° de version dans le local storage
-localStorage.setItem('appVersion', VERSION_SC)
-
+const VERSION_SC = "2.6.1"
 
 // Based off of https://github.com/pwa-builder/PWABuilder/blob/main/docs/sw.js
 
