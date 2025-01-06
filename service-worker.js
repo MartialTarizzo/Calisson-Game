0af1e51d67eddc8b617c64fdf5f887056baf8e23
Martial <martial.tarizzo@gmail.com>
2025-01-06 18:33:55 +0100

    V2.14.1 style séparé pour les pages d'index
    	new file:   css/index.css
    	modified:   index.en.html
    	modified:   index.html
    	modified:   service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,7 @@
 
 // Le n° de version de l'appication PWA
 // Mettre à jour pour forcer l'actualisation du cache du navigateur
-const VERSION_SC = "2.14.0"
+const VERSION_SC = "2.14.1"
 
 // Based off of https://github.com/pwa-builder/PWABuilder/blob/main/docs/sw.js
 
