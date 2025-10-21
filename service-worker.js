e2f18ab3d8431f3bf0fc460678db477fcad62979
Martial <martial.tarizzo@gmail.com>
2025-10-21 21:51:40 +0200

    V2.16.0 MAJ n° version

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,7 @@
 
 // Le n° de version de l'appication PWA
 // Mettre à jour pour forcer l'actualisation du cache du navigateur
-const VERSION_SC = "2.15.8"
+const VERSION_SC = "2.16.0"
 
 // Based off of https://github.com/pwa-builder/PWABuilder/blob/main/docs/sw.js
 
